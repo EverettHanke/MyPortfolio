8da25a7c3bb7400eb4761a459a9ab3d613600031
EverettHanke <forgetekbusiness@gmail.com>
2025-08-17 14:26:31 -0700

    putting it all together

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -6,10 +6,24 @@ import '../../slideVFX.css';
 import '../../scrollerVFX.css';
 import '../../mobileStyles.css';
 
+import Header from './Header';
+import Hero from './Hero';
+import About from './About';
+import Projects from './Projects';
+import Skills from './Skills';
+import Contact from './Contact';
+import Footer from './Footer';
+
 function App() {
   return (
     <div>
-      {/* We will add the components here */}
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 }
